5ffd7cc627404af5285fe312078227ccfd518bdf
SoegiDev <soegidev.id@gmail.com>
2024-06-19 02:18:36 +0700

    TEST BINANCE step 3 balance

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! BOT TRADE ALREADY</h1>"
+    return "<h1 style='color:blue'>Hello There! BOT TRADE ALREADY STEP ACCOUNT BALANCES</h1>"
 
 
 @app.route('/initdb')
